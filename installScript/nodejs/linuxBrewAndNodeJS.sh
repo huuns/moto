fbfc91b3f1f41b5ab3005064e7ecdfb5a997fd0d
motobyus <lainer23@naver.com>
2016-02-13 21:19:34 +0900

    up

---
+++ installScript/nodejs/linuxBrewAndNodeJS.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "brew Install Ubuntu Version\n"
+echo "brew Install Ubuntu Version\n"
+apt-get install -y build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+su - ubuntu -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"'
+echo "brew Install end\n"
+echo "brew Install end\n"
+echo "\n\n\n\n\n\n"
+echo "NodeJS Install Ubuntu Version\n"
+echo "NodeJS Install Ubuntu Version\n"
+
+
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+echo "brew install node.js"
+
+
+echo "NodeJS Install end\n"
+echo "NodeJS Install end\n"
+
