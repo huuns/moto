88eac76ff76e3345b9874b9cc92f8db7d350c0c2
moto <lainer2341@gmail.com>
2016-08-22 12:51:49 +0900

    Add : postgresql 9.5 installation

---
+++ install/postgresql9.5/2.install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt-get install -y postgresql-client-9.5 postgresql-9.5 postgresql-9.5-postgis-2.2 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 postgresql-contrib-9.5
+
+
