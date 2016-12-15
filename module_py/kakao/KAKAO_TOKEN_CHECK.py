aa7f122d7a8a50715ec22eed7a118530605c5a64
moto <lainer2341@gmail.com>
2016-12-15 20:39:39 +0900

    update

--- module_py/kakao/KAKAO_TOKEN_CHECK.py
+++ module_py/kakao/KAKAO_TOKEN_CHECK.py
@@ -13,3 +13,14 @@ class KAKAO_ID_TOKEN_CHECK:
         else:
             return False
 
+
+    # GET /v1/user/ids HTTP/1.1
+    # Host: kapi.kakao.com
+    # Authorization: KakaoAK {admin_key}
+    # Content-type: application/x-www-form-urlencoded;charset=utf-8
+    def get_kakao_user_list(self, adminToken):
+        endpoint = "https://kapi.kakao.com/v1/user/ids"
+        headers  = {"Authorization":"KakaoAK %s" % (adminToken) }
+        userCount = requests.get(endpoint,headers=headers).json()["total_count"]
+        return userCount
+        
